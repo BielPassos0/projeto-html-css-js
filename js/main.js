aa45e7037fea9a56418115a45862d4cfe482d7b1
Alex Alves Moraes <alex_moraes120@hotmail.com>
2022-02-14 15:29:08 -0300

    slickSlide

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-let botao = document.querySelector('.menu-principal__btn');
-let menuPrincipal = document.querySelector('.menu-principal');
-botao.addEventListener("click", abreFechaMenu);
-
-function abreFechaMenu(evento){
-    menuPrincipal.classList.toggle('menu-principal--fechado');
-}
+//JavaScriptPuro
+// let botao = document.querySelector('.menu-principal__btn');
+// let menuPrincipal = document.querySelector('.menu-principal');
+// botao.addEventListener("click", abreFechaMenu);
+// function abreFechaMenu(evento) {
+// menuPrincipal.classList.toggle('menu-principal--fechado');
+//}
+// jQuery
+$(document).ready(function () {
+    // Botao do menu
+    $('.menu-principal__btn').click(function () {
+        $('.menu-principal').toggleClass('menu-principal--fechado');
+    });
+    // Slide Depoimentos    
+    $('.depoimentos__caixa').slick();
+});
