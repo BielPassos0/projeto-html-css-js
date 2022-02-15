799b232f2751c11ef92f64edc48e05bdd6303853
Alex Alves Moraes <alex_moraes120@hotmail.com>
2022-02-15 16:14:11 -0300

    Jquery-Validate

--- js/main.js
+++ js/main.js
@@ -12,5 +12,36 @@ $(document).ready(function () {
         $('.menu-principal').toggleClass('menu-principal--fechado');
     });
     // Slide Depoimentos    
-    $('.depoimentos__caixa').slick();
+    $('.depoimentos__caixa').slick({
+        autoplay: true,
+        arrows: false,
+        dots: true,
+    });
+    // Validaçao do formulario de contato
+    $('.formulario').validate({
+        rules: {
+            nome: 'required',
+            email: {
+                required: true,
+                email: true,
+            },
+            mensagem: {
+                required: false,
+                email: true,
+            },
+        },
+        messages: {
+            nome: 'Por favor preencha o campo com seu nome!',
+            email: {
+                required: "Por Favor preencha o e-mail!",
+                email: "Por favor preencha um e-mail válido!",
+            },
+            mensagem: 'Este campo é opicional',
+        }
+    });
+});
+
+$('#cep').blur(async (elemento) => {
+    var result = await $.get(`https://viacep.com.br/ws/${elemento.target.value}/json/`);
+    $('#end').val(result.logradouro);
 });
